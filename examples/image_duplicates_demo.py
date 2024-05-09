0946a32a23f368bb9e8c49fa15b24d2c9e7fd1ca
Larry Zhang <larryzh@blitzcorp.com>
2024-05-09 13:06:32 +0800

    Code formating.

--- examples/image_duplicates_demo.py
+++ examples/image_duplicates_demo.py
@@ -3,38 +3,47 @@
 @author:XuMing(xuming624@qq.com)
 @description: 
 """
-import sys
 
+import sys
+from pathlib import Path
 from PIL import Image
 
-sys.path.append('..')
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(BASE_DIR))
+
 from similarities import ClipSimilarity, paraphrase_mining_embeddings
 
 
 def load_data(file_path):
     data_paths = []
     c = 0
-    with open(file_path, 'r', encoding='utf-8') as f:
+
+    with open(file_path, "r", encoding="utf-8") as f:
         for line in f:
             line = line.strip()
             c += 1
+
             if c == 1:
                 continue
             if line:
-                path = line.split(',')[0]
+                path = line.split(",")[0]
                 data_paths.append(path)
+
     return data_paths
 
 
 def main():
-    corpus_paths = load_data('data/image_info.csv')
+    corpus_paths = load_data("%s/examples/data/image_info.csv" % BASE_DIR)
     corpus_paths = list(set(corpus_paths))
-    print('corpus size:', len(corpus_paths), 'top3:', corpus_paths[:3])
+    print("corpus size:", len(corpus_paths), "top3:", corpus_paths[:3])
 
     model = ClipSimilarity()
     print(model)
+
     corpus = [Image.open(i) for i in corpus_paths]
-    corpus_embeddings = model.get_embeddings(corpus, show_progress_bar=True, convert_to_tensor=True)
+    corpus_embeddings = model.get_embeddings(
+        corpus, show_progress_bar=True, convert_to_tensor=True
+    )
     duplicates = paraphrase_mining_embeddings(corpus_embeddings)
 
     for score, idx1, idx2 in duplicates[0:10]:
@@ -55,5 +64,5 @@ def main():
     # data/image12-like-image1.png
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- examples/image_semantic_search_demo.py
+++ examples/image_semantic_search_demo.py
@@ -5,65 +5,78 @@
 """
 import glob
 import sys
-
 from PIL import Image
 
-sys.path.append('..')
+sys.path.append("..")
 from similarities import ImageHashSimilarity, SiftSimilarity, ClipSimilarity
 
 
 def sim_and_search(m):
+    image_fps1 = ["data/image1.png", "data/image3.png"]
+    image_fps2 = ["data/image12-like-image1.png", "data/image10.png"]
+
+    imgs1 = [Image.open(i) for i in image_fps1]
+    imgs2 = [Image.open(i) for i in image_fps2]
+
+    corpus_fps = glob.glob("data/*.jpg") + glob.glob("data/*.png")
+    corpus_imgs = [Image.open(i) for i in corpus_fps]
+
     print(m)
-    # similarity
+
+    # Similarity
     sim_scores = m.similarity(imgs1, imgs2)
-    print('sim scores: ', sim_scores)
+    print("sim scores: ", sim_scores)
+
     for (idx, i), j in zip(enumerate(image_fps1), image_fps2):
         s = sim_scores[idx] if isinstance(sim_scores, list) else sim_scores[idx][idx]
         print(f"{i} vs {j}, score: {s:.4f}")
-    # search
+
+    # Search
     m.add_corpus(corpus_imgs)
     queries = imgs1
     res = m.most_similar(queries, topn=3)
-    print('sim search: ', res)
+    print("Similarity search: ", res)
+
     for q_id, c in res.items():
-        print('query:', image_fps1[q_id])
-        print("search top 3:")
+        print("Query:", image_fps1[q_id])
+        print("Search top 3:")
+
         for corpus_id, s in c.items():
-            print(f'\t{m.corpus[corpus_id].filename}: {s:.4f}')
-    print('-' * 50 + '\n')
+            print(f"\t{m.corpus[corpus_id].filename}: {s:.4f}")
+
+    print("-" * 50 + "\n")
 
 
 def clip_demo():
+    # English model name: openai/clip-vit-base-patch32
     m = ClipSimilarity(model_name_or_path="OFA-Sys/chinese-clip-vit-base-patch16")
-    # english model name: openai/clip-vit-base-patch32
     print(m)
-    # similarity score between text and image
+
+    # Similarity score between text and image
     image_fps = [
-        'data/image3.png',  # yellow flower image
-        'data/image1.png',  # tiger image
+        "data/image3.png",  # Yellow flower image
+        "data/image1.png",  # Tiger image
     ]
-    texts = ['a yellow flower', '老虎', '一头狮子', '玩具车']
+
+    texts = ["a yellow flower", "老虎", "一头狮子", "玩具车"]
     imgs = [Image.open(i) for i in image_fps]
+
     sim_scores = m.similarity(imgs, texts)
-    print('sim scores: ', sim_scores)
+    print("Similarity scores: ", sim_scores)
+
     for idx, i in enumerate(image_fps):
         for idy, j in enumerate(texts):
             s = sim_scores[idx][idy]
             print(f"{i} vs {j}, score: {s:.4f}")
-    print('-' * 50 + '\n')
+
+    print("-" * 50 + "\n")
 
 
 if __name__ == "__main__":
-    image_fps1 = ['data/image1.png', 'data/image3.png']
-    image_fps2 = ['data/image12-like-image1.png', 'data/image10.png']
-    imgs1 = [Image.open(i) for i in image_fps1]
-    imgs2 = [Image.open(i) for i in image_fps2]
-    corpus_fps = glob.glob('data/*.jpg') + glob.glob('data/*.png')
-    corpus_imgs = [Image.open(i) for i in corpus_fps]
-    # 1. image and text similarity
+    # Image and text similarity
     clip_demo()
 
-    # 2. image and image similarity score
-    sim_and_search(ClipSimilarity())  # the best result
-    sim_and_search(ImageHashSimilarity(hash_function='phash'))
+    # Image and image similarity score
+    sim_and_search(ClipSimilarity())
+    sim_and_search(ImageHashSimilarity(hash_function="phash"))
     sim_and_search(SiftSimilarity())
